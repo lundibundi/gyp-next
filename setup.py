5145c43bb7cf75f6c8d5db2f5afbd1a1a9158ac9
Ujjwal Sharma <ryzokuken@disroot.org>
2020-03-15 23:24:47 +0530

    meta: update setup.py for pypi

--- setup.py
+++ setup.py
@@ -4,15 +4,24 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from os import path
+
 from setuptools import setup
 
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, "README.md")) as in_file:
+    long_description = in_file.read()
+
 setup(
-    name="gyp",
-    version="0.1",
-    description="Generate Your Projects",
-    author="Chromium Authors",
-    author_email="chromium-dev@googlegroups.com",
-    url="http://code.google.com/p/gyp",
+    name="gyp-next",
+    version="0.1.1",
+    description="A fork of the GYP build system for use in the Node.js projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Ujjwal Sharma",
+    author_email="ryzokuken@disroot.org",
+    url="https://github.com/nodejs/gyp-next",
     package_dir={"": "pylib"},
     packages=["gyp", "gyp.generator"],
     entry_points={"console_scripts": ["gyp=gyp:script_main"]},
